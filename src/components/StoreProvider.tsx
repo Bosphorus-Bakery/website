f14b80048fc93d214f21b91622fd5b5f5efdbe6d
akbarimo <mo.akbari15@gmail.com>
2024-05-16 00:06:16 -0700

    Refactor StoreProvider imports

--- src/components/StoreProvider.tsx
+++ src/components/StoreProvider.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import * as React from "react";
+import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "@/lib";
-import { useRef } from "react";
 
-const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+const StoreProvider = ({ children }: { children: ReactNode }) => {
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     storeRef.current = makeStore();
