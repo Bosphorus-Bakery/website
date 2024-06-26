7e942568239c7ad810fdff63aefa35b708e9571e
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2024-06-25 22:27:39 -0700

    Add compiled Contact.js form - Add Contact form data shape

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Contact = function () {
+    return <div>Hello Contact</div>;
+};
+exports.default = Contact;
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,3 +1,12 @@
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: number;
+  subject: string;
+  description: string;
+}
+
 const Contact = () => {
   return <div>Hello Contact</div>;
 };
