9808d2373ae3aa3b01c67c41efe07f606783a357
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2024-09-01 07:12:40 -0700

    Clean up dev dependencies

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,6 +1,9 @@
-import { useDispatch, useSelector, useStore } from "react-redux";
-import type { RootState, AppDispatch, AppStore } from "@/lib";
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import type { RootState, AppDispatch, AppStore } from '@/lib';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
+
+const test = 'test';
+const test2 = '';
