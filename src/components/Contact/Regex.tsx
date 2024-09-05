764441bed1fd532cd709c04595a8a59f1f38cf4f
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2024-09-04 18:20:41 -0700

    Email regex pattern

--- src/components/Contact/Regex.tsx
+++ src/components/Contact/Regex.tsx
@@ -1,4 +1,6 @@
+// TO DO: Regex patterns for name, email, phone, subj, description
 // '^' amd '$' marks start and end of match respectively
 export const nameRegex: RegExp = /^[A-Za-zÀ-ÖØ-öø-ÿ]{0,50}$/;
 
-// TO DO: Regex patterns for name, email, phone, subj, description
+// Matches any alphanumberic, alphanumbers and special chars, @, lowercase chars, ., upper and lower-case alphabetic chars (min 2)
+export const emailRegex: RegExp = /^[A-Za-z0-9][A-Za-z0-9\.\_\-\+\%]+@[a-z]\.[a-zA-Z]{2,}$/;
