80a885949a177e4ede9b3b007381ed552c201034
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2025-01-19 13:49:51 -0800

    Add store details and resizing form on Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,9 +9,31 @@ export const metadata: Metadata = {
 const ContactPage = () => {
   return (
     <div>
-      <h1 className='form-headings'>Contact Us</h1>
-      <p className='form-subheadings'>Please fill out the form below to reach out to us</p>
-      <ContactForm />
+      <h1 className="form-headings">Contact Us</h1>
+      <p className="form-subheadings">Please fill out the form below to reach out to us</p>
+      <div className="contact-page-content">
+        <ContactForm />
+        <div className="contact-store-details">
+          <div id="store-address">
+            <p>1301 Maurice Avenue</p>
+            <p>Rohnert Park, CA 94928</p>
+          </div>
+          <div id="store-hours">
+            <p>OPEN</p>
+            <p>Monday to Thursday</p>
+          </div>
+          <div id="store-phone-email">
+            <p>(415)408-3037</p>
+            <p>owner@bosphorusbakery.com</p>
+          </div> 
+          <img
+          src="/public/placeholder-image-baklava.jpg"
+          alt="Baklava"
+        />
+        </div>
+
+      </div>
+
     </div>
   )
 }
