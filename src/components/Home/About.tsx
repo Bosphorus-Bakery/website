cfbcfc8ed3d8731a3748950a7ad1fcfe4f9be65a
Mo <mo.akbari15@gmail.com>
2026-06-02 02:07:27 -0700

    Style home page cards

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -1,9 +1,16 @@
+import { HomeStyles } from '@/styles';
+import Link from 'next/link';
+
 const About = () => {
   return (
-    <div className="home-page-about">
-      <h3 className="home-page-about-heading"></h3>
-      <p className="home-page-about-text"></p>
-      <button className="home-page-about-cta primary-button"></button>
+    <div className={HomeStyles.card}>
+      <h3 className={HomeStyles.heading}>About Us</h3>
+      <p className={HomeStyles.text}>
+        Since 2003, our family has brought an iconic piece of Mediterranean culture to Northern California. From the Bosphorus to the Golden Gate, our journey has always been a symbol of East meets West — bringing people together, one piece of baklava at a time.
+      </p>
+      <Link href="/about" className={HomeStyles.cta}>
+        Our Story
+      </Link>
     </div>
   );
 };
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
 import formStyles from "./ContactForm.module.css";
 import contactStyles from "./Contact.module.css";
+import HomeStyles from "./Home.module.css";
 
-export { formStyles, contactStyles };
+export { formStyles, contactStyles, HomeStyles };
