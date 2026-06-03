f8ec55e2c846477e097c3920bf26b074731918f1
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2026-06-02 19:52:04 -0700

    Styling for store details card; Error messages

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,26 +6,42 @@ const ContactPage = () => {
     <div className={contactStyles['page-container']}>
       <h1 className={contactStyles['heading-1']}>Contact Us</h1>
       <p className={contactStyles['subheading']}>
-        Please fill out the form below to reach out to us
+        Send us a message and we'll get back to you shortly
       </p>
       <div className={contactStyles['content-container']}>
         <div className={contactStyles['form-panel']}>
           <ContactForm />
         </div>
+
         <div className={contactStyles['store-details-container']}>
-          <div className={contactStyles['store-detail']} id="store-address">
-            <p>
-              1301 Maurice Avenue
-              <br />
-              Rohnert Park, CA 94928
-            </p>
+          <div className={contactStyles['store-detail-row']}>
+            <span className={contactStyles['detail-label']}>Address</span>
+            <div className={contactStyles['detail-value']}>
+              <p>1301 Maurice Avenue Rohnert Park, CA 94928</p>
+            </div>
           </div>
-          <div className={contactStyles['store-detail']} id="store-hours">
-            <p>Monday to Thursday</p>
+
+          <div className={contactStyles['store-detail-row']}>
+            <span className={contactStyles['detail-label']}>Hours</span>
+            <div className={contactStyles['detail-value']}>
+              <p>Mon - Thurs 9:00am - 2:00pm</p>
+            </div>
           </div>
-          <div className={contactStyles['store-detail']} id="store-phone-email">
-            <p>(415)408-3037</p>
-            <p>owner@bosphorusbakery.com</p>
+
+          <div className={contactStyles['store-detail-row']}>
+            <span className={contactStyles['detail-label']}>Phone</span>
+            <div className={contactStyles['detail-value']}>
+              <a href="tel:4154083037">(415) 408-3037</a>
+            </div>
+          </div>
+
+          <div className={contactStyles['store-detail-row']}>
+            <span className={contactStyles['detail-label']}>Email</span>
+            <div className={contactStyles['detail-value']}>
+              <a href="mailto:owner@bosphorusbakery.com">
+                owner@bosphorusbakery.com
+              </a>
+            </div>
           </div>
         </div>
       </div>
